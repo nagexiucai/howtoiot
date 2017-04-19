6ad0b13165de7f5c75b524d45cf374fad436afd9
nagexiucai <nagexiucai@qq.com>
2017-04-20 00:31:20 +0800

    build stm32f103x's basic project with uart&i2c upon rtx

---
+++ boards/v1_2_kylinmini_open_iot_10086_cn/RTE/RTE_Components.h
@@ -0,0 +1,23 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'kylinmini' 
+ * Target:  'humiture-monitor' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+#define RTE_CMSIS_RTOS                  /* CMSIS-RTOS */
+        #define RTE_CMSIS_RTOS_RTX              /* CMSIS-RTOS Keil RTX */
+#define RTE_Drivers_I2C1                /* Driver I2C1 */
+        #define RTE_Drivers_I2C2                /* Driver I2C2 */
+#define RTE_Drivers_UART1               /* Driver UART1 */
+        #define RTE_Drivers_UART2               /* Driver UART2 */
+        #define RTE_Drivers_UART3               /* Driver UART3 */
+        #define RTE_Drivers_UART4               /* Driver UART4 */
+        #define RTE_Drivers_UART5               /* Driver UART5 */
+
+#endif /* RTE_COMPONENTS_H */
